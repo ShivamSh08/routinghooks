2771b6c40766b74fbe180f4940249bd19b5f0a61
ShivamSh08 <113332920+ShivamSh08@users.noreply.github.com>
2024-07-08 06:48:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import AboutUs from './components/AboutUs';
+import ContactUs from './components/ContactUs';
+import CEO from './components/CEO';
+import CFO from './components/CFO';
+import PageNotFound from './components/PageNotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+      <h1> Understanding Routing Hooks</h1>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="about" element={<AboutUs/>} />
+        <Route path="contact" element={<ContactUs/>} />
+        <Route path="ceo" element={<CEO/>} />
+        <Route path="cfo" element={<CFO/>} />
+        <Route path="*" element={<PageNotFound/>}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/PageNotFound.js
@@ -0,0 +1,9 @@
+const PageNotFound = () => {
+    return (
+        <>
+        <h3 className="bg-warning mt-4 pt-4"> Sorry,we could not find the page that you are looking for!</h3>
+        </>
+     );
+}
+ 
+export default PageNotFound;
